39fe3fbab15caaa0ebf8ddd6a24593fc8655d629
Luciano D'Agata <martinvilla481@gmail.com>
2018-10-11 16:38:46 -0300

    elimine el main.js no sirve para nada

--- js/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-let inicio = document.querySelector(".inicio");
-inicio.addEventListener("click", function(e) {
-    // e.preventDefault();
-    inicio.classList.toggle("active");
-});
